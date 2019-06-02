a2d55e798388377347cf3f9aa599007cdca1c440
FuryOFFools <AlderFrond@gmail.com>
2019-06-02 18:35:54 +0400

    IBlock

--- Block/Block.cs
+++ Block/Block.cs
@@ -2,17 +2,6 @@
 
 namespace BC
 {
-    public interface IBlock
-    {
-        byte[] Data { get; }
-        byte[] Hash { get; set; }
-        int Nonce { get; set; }
-        byte[] PrevHash { get; set; }
-        DateTime TimeStamp { get; }
-
-        string ToString();
-    }
-
     [Serializable]
     public class Block : IBlock
     {
---
+++ Block/IBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BC
+{
+    public interface IBlock
+    {
+        byte[] Data { get; }
+        byte[] Hash { get; set; }
+        int Nonce { get; set; }
+        byte[] PrevHash { get; set; }
+        DateTime TimeStamp { get; }
+
+        string ToString();
+    }
+}
